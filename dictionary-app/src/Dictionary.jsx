106083894ef12e53abc2c2975f8d7feacbdb37f3
Alicia Buelow <alicia@buelow.com.au>
2026-03-06 21:04:06 +0800

    Installed axios, integrated API

--- dictionary-app/src/Dictionary.jsx
+++ dictionary-app/src/Dictionary.jsx
@@ -1,11 +1,20 @@
 import { useState } from 'react';
 import './Dictionary.css'
+import axios from 'axios';
 
 export default function Dictionary() {
     const [keyword, setKeyword] = useState(null);
 
+    function fetchData (response) {
+        console.log(response.data);
+    }
+
     function handleSearch(event) {
         event.preventDefault();
+
+        const apiKey = "04dbc8004716437tab5bc0bfo1baf277"
+        const apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`
+        axios.get(apiUrl).then(fetchData);
     }
 
     function handleSearchQuery(event) {
